a7f4c03eaef4a3699638dcae443684e0d762bdfb
최은관 <silvergwan@gmail.com>
2025-11-01 22:29:10 +0900

    test :: 커밋 확인

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import todosRoutes from "./todos/routes/todos";
 
+//윈도우
 dotenv.config();
 const app = express();
 app.use(express.json());
